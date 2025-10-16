4a5733ffd5e17a2d4343d394963162e01b758ab9
Lance <dogmandevices@gmail.com>
2025-10-16 00:32:05 -0400

    set up tremolo rate and start setting up tremolo depth with placeholder sine wave

--- src/Water.cpp
+++ src/Water.cpp
@@ -1,5 +1,6 @@
 #include "plugin.hpp"
 
+float blinkPhaseT = 0.f;
 
 struct Water : Module {
 	enum ParamId {
@@ -9,15 +10,12 @@ struct Water : Module {
 		CHORUSCV_PARAM,
 		RATECV_PARAM,
 		TREMOLOCV_PARAM,
-		BLEND_PARAM,
-		BLENDCV_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
 		CHORUSCV_INPUT,
 		RATECV_INPUT,
 		TREMOLOCV_INPUT,
-		BLENDCV_INPUT,
 		AUDIO_INPUT,
 		INPUTS_LEN
 	};
@@ -26,29 +24,43 @@ struct Water : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		RATE_LIGHT,
+		CHORUSRATE_LIGHT,
+		TREMOLORATE_LIGHT,
 		LIGHTS_LEN
 	};
+
+	float tPhase = 0.5f;
 
 	Water() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(CHORUSDEPTH_PARAM, 0.f, 1.f, 0.f, "Chorus Depth");
-		configParam(RATE_PARAM, 0.f, 1.f, 0.f, "Chorus and Tremolo Rate");
-		configParam(TREMOLODEPTH_PARAM, 0.f, 1.f, 0.f, "Tremolo Depth");
+		configParam(RATE_PARAM, -4.f, 5.f, 1.f, "Chorus and Tremolo Rate", " Hz", 2, 1);
+		configParam(TREMOLODEPTH_PARAM, 0.f, 1.f, 0.5f, "Tremolo Depth", "%", 0, 100);
 		configParam(CHORUSCV_PARAM, 0.f, 1.f, 0.f, "Chorus Depth CV");
 		configParam(RATECV_PARAM, 0.f, 1.f, 0.f, "Rate CV");
 		configParam(TREMOLOCV_PARAM, 0.f, 1.f, 0.f, "Tremolo Depth CV");
-		configParam(BLEND_PARAM, 0.f, 1.f, 0.f, "Blend");
-		configParam(BLENDCV_PARAM, 0.f, 1.f, 0.f, "Blend CV");
 		configInput(CHORUSCV_INPUT, "Chorus Depth CV");
 		configInput(RATECV_INPUT, "Chorus and Tremolo Rate CV");
 		configInput(TREMOLOCV_INPUT, "Tremolo Depth CV");
-		configInput(BLENDCV_INPUT, "Blend CV");
 		configInput(AUDIO_INPUT, "Audio");
 		configOutput(AUDIO_OUTPUT, "Audio");
 	}
 
 	void process(const ProcessArgs& args) override {
+		float tDepth = params[TREMOLODEPTH_PARAM].getValue();
+
+		float in = inputs[AUDIO_INPUT].getVoltage();
+
+		float tRate = params[RATE_PARAM].getValue();
+		float tFreq = std::pow(2.f, tRate);
+		// tRate = dsp::exp2_taylor5(tRate);
+		tPhase += tFreq * args.sampleTime;
+		if (tPhase > 1.f)
+			tPhase -= 1.f;
+		float tLFO = std::sin(2.f * M_PI * tPhase);
+
+
+		lights[TREMOLORATE_LIGHT].setBrightness(tLFO);
 	}
 };
 
@@ -63,24 +75,22 @@ struct WaterWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(16.484, 30.652)), module, Water::CHORUSDEPTH_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(45.704, 31.037)), module, Water::RATE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(75.967, 30.875)), module, Water::TREMOLODEPTH_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.019, 45.679)), module, Water::CHORUSCV_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(38.536, 45.719)), module, Water::RATECV_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(69.133, 45.865)), module, Water::TREMOLOCV_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(74.485, 88.419)), module, Water::BLEND_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(80.668, 102.912)), module, Water::BLENDCV_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(16.484, 30.652)), module, Water::CHORUSDEPTH_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(45.704, 31.037)), module, Water::RATE_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(75.967, 30.875)), module, Water::TREMOLODEPTH_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(10.019, 45.679)), module, Water::CHORUSCV_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(38.536, 45.719)), module, Water::RATECV_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(69.133, 45.865)), module, Water::TREMOLOCV_PARAM));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(24.071, 45.395)), module, Water::CHORUSCV_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(52.314, 45.561)), module, Water::RATECV_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(82.549, 45.827)), module, Water::TREMOLOCV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(66.214, 102.55)), module, Water::BLENDCV_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(66.378, 116.484)), module, Water::AUDIO_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(79.314, 116.564)), module, Water::AUDIO_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(12.264, 106.205)), module, Water::RATE_LIGHT));
+		addChild(createLightCentered<MediumLight<BlueLight>>(mm2px(Vec(32.26, 21.675)), module, Water::CHORUSRATE_LIGHT));
+		addChild(createLightCentered<MediumLight<BlueLight>>(mm2px(Vec(60.143, 21.997)), module, Water::TREMOLORATE_LIGHT));
 	}
 };
 
