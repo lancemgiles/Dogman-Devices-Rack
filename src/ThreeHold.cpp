77729fcf73d3717c951eeaab4ce623b76921c41f
Lance <dogmandevices@gmail.com>
2025-11-13 00:39:42 -0600

    refine logprob and threehold panels; implement s&h for outputs 2 and 3

--- src/ThreeHold.cpp
+++ src/ThreeHold.cpp
@@ -37,14 +37,14 @@ struct ThreeHold : Module {
 	dsp::SchmittTrigger trigger3[maxPolyphony];
 
 	float sig1[maxPolyphony] = {0.0};
-	float sig2 = 0.0;
-	float sig3 = 0.0;
+	float sig2[maxPolyphony] = {0.0};
+	float sig3[maxPolyphony] = {0.0};
 
 	ThreeHold() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(SCALE1_PARAM, 0.f, 1.f, 1.f, "Scale Input 1", "%", 0, 100);
-		configParam(SCALE2_PARAM, 0.f, 1.f, 0.f, "Scale Input 2", "%", 0, 100);
-		configParam(SCALE3_PARAM, 0.f, 1.f, 0.f, "Scale Input 3", "%", 0, 100);
+		configParam(SCALE2_PARAM, 0.f, 1.f, 1.f, "Scale Input 2", "%", 0, 100);
+		configParam(SCALE3_PARAM, 0.f, 1.f, 1.f, "Scale Input 3", "%", 0, 100);
 		configInput(SIG1_INPUT, "Signal 1");
 		configInput(TRIG1_INPUT, "Trigger 1");
 		configInput(SIG2_INPUT, "Signal 2");
@@ -58,7 +58,13 @@ struct ThreeHold : Module {
 
 	void process(const ProcessArgs& args) override {
 		currentPolyphony1 = std::max(1, inputs[SIG1_INPUT].getChannels());
+		currentPolyphony2 = std::max(1, inputs[SIG2_INPUT].getChannels());
+		currentPolyphony3 = std::max(1, inputs[SIG3_INPUT].getChannels());
+
 		outputs[OUT1_OUTPUT].setChannels(currentPolyphony1);
+		outputs[OUT2_OUTPUT].setChannels(currentPolyphony2);
+		outputs[OUT3_OUTPUT].setChannels(currentPolyphony3);
+
 		for (int c = 0; c < currentPolyphony1; c ++) {
 			if (trigger1[c].process(inputs[TRIG1_INPUT].getPolyVoltage(c) / 0.7)) {
 				sig1[c] = inputs[SIG1_INPUT].getVoltage(c) * params[SCALE1_PARAM].getValue();
@@ -66,7 +72,18 @@ struct ThreeHold : Module {
 			}
 		}
 
-
+		for (int c = 0; c < currentPolyphony2; c ++) {
+			if (trigger2[c].process(inputs[TRIG2_INPUT].getPolyVoltage(c) / 0.7)) {
+				sig2[c] = inputs[SIG2_INPUT].getVoltage(c) * params[SCALE2_PARAM].getValue();
+				outputs[OUT2_OUTPUT].setVoltage(sig2[c], c);
+			}
+		}
+		for (int c = 0; c < currentPolyphony3; c ++) {
+			if (trigger3[c].process(inputs[TRIG3_INPUT].getPolyVoltage(c) / 0.7)) {
+				sig3[c] = inputs[SIG3_INPUT].getVoltage(c) * params[SCALE3_PARAM].getValue();
+				outputs[OUT3_OUTPUT].setVoltage(sig3[c], c);
+			}
+		}
 
 	}
 };
@@ -77,14 +94,12 @@ struct ThreeHoldWidget : ModuleWidget {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/ThreeHold.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.023, 20.079)), module, ThreeHold::SCALE1_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.023, 51.3)), module, ThreeHold::SCALE2_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.023, 83.05)), module, ThreeHold::SCALE3_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(22.023, 20.079)), module, ThreeHold::SCALE1_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(22.023, 51.3)), module, ThreeHold::SCALE2_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(22.023, 83.05)), module, ThreeHold::SCALE3_PARAM));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.241, 20.079)), module, ThreeHold::SIG1_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.241, 32.974)), module, ThreeHold::TRIG1_INPUT));
