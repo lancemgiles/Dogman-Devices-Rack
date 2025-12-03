a50643d2267e56f336c8d728ceb22e5bff148a9b
Lance <dogmandevices@gmail.com>
2025-12-03 01:59:29 -0600

    update panel; implement first delay

--- src/TriParDel.cpp
+++ src/TriParDel.cpp
@@ -31,6 +31,7 @@ struct TriParDel : Module {
         ONE_OUTPUT,
         TWO_OUTPUT,
         THREE_OUTPUT,
+        MIXED_OUTPUT,
         POLY_OUTPUT,
 		OUTPUTS_LEN
 	};
@@ -92,6 +93,8 @@ struct TriParDel : Module {
         configOutput(ONE_OUTPUT, "Audio 1");
         configOutput(TWO_OUTPUT, "Audio 2");
         configOutput(THREE_OUTPUT, "Audio 3");
+        configOutput(MIXED_OUTPUT, "Mixed Audio");
+        configOutput(POLY_OUTPUT, "Polyphonic");
 
         configBypass(ONE_INPUT, ONE_OUTPUT);
         configBypass(TWO_INPUT, TWO_OUTPUT);
@@ -106,6 +109,92 @@ struct TriParDel : Module {
 	}
 
 	void process(const ProcessArgs& args) override {
+        clockFreq = 2.f;
+        // Get input to delay block
+		float in = inputs[ONE_INPUT].getVoltageSum();
+		float feedback = params[FEEDBACK1_PARAM].getValue(); // + inputs[FEEDBACK1_INPUT].getVoltage() / 10.f;
+		feedback = clamp(feedback, 0.f, 1.f);
+		float dry = in + lastWet * feedback;
+
+		// Compute freq
+		// Scale time knob to 1V/oct pitch based on formula explained in constructor, for backwards compatibility
+		float pitch = std::log2(1000.f) - std::log2(10000.f) * params[TIME1_PARAM].getValue();
+		pitch += inputs[TIMECV1_INPUT].getVoltage() * params[TIMECV1_PARAM].getValue();
+		float freq = clockFreq / 2.f * dsp::exp2_taylor5(pitch);
+		// Number of desired delay samples
+		float index = args.sampleRate / freq;
+		// In order to delay accurate samples, subtract by the historyBuffer size, and an experimentally tweaked amount.
+		index -= 16 + 4.f;
+		index = clamp(index, 2.f, float(HISTORY_SIZE - 1));
+		// DEBUG("freq %f index %f", freq, index);
+
+
+		// Push dry sample into history buffer
+		if (!historyBuffer.full()) {
+			historyBuffer.push(dry);
+		}
+
+		if (outBuffer.empty()) {
+			// How many samples do we need consume to catch up?
+			float consume = index - historyBuffer.size();
+			double ratio = std::pow(4.f, clamp(consume / 10000.f, -1.f, 1.f));
+			// DEBUG("index %f historyBuffer %lu consume %f ratio %lf", index, historyBuffer.size(), consume, ratio);
+
+			// Convert samples from the historyBuffer to catch up or slow down so `index` and `historyBuffer.size()` eventually match approximately
+			SRC_DATA srcData = {};
+			srcData.data_in = (const float*) historyBuffer.startData();
+			srcData.data_out = (float*) outBuffer.endData();
+			srcData.input_frames = std::min((int) historyBuffer.size(), 16);
+			srcData.output_frames = outBuffer.capacity();
+			srcData.end_of_input = false;
+			srcData.src_ratio = ratio;
+			if (src)
+				src_process(src, &srcData);
+			historyBuffer.startIncr(srcData.input_frames_used);
+			outBuffer.endIncr(srcData.output_frames_gen);
+			// DEBUG("used %ld gen %ld", srcData.input_frames_used, srcData.output_frames_gen);
+		}
+
+		float wet = 0.f;
+		if (!outBuffer.empty()) {
+			wet = outBuffer.shift();
+		}
+		wet = clamp(wet, -100.f, 100.f);
+
+		// Apply color to delay wet output
+		// float color = params[TONE_PARAM].getValue() + inputs[TONE_INPUT].getVoltage() / 10.f * params[TONE_CV_PARAM].getValue();
+		// color = clamp(color, 0.f, 1.f);
+		// float colorFreq = std::pow(100.f, 2.f * color - 1.f);
+
+		// float lowpassFreq = clamp(20000.f * colorFreq, 20.f, 20000.f);
+		// lowpassFilter.setCutoffFreq(lowpassFreq / args.sampleRate);
+		// lowpassFilter.process(wet);
+		// wet = lowpassFilter.lowpass();
+
+		// float highpassFreq = clamp(20.f * colorFreq, 20.f, 20000.f);
+		// highpassFilter.setCutoff(highpassFreq / args.sampleRate);
+		// highpassFilter.process(wet);
+		// wet = highpassFilter.highpass();
+
+		// Set wet output
+		//outputs[WET_OUTPUT].setVoltage(wet);
+		lastWet = wet;
+
+		// Set mix output
+		float mix = params[MIX1_PARAM].getValue(); // + inputs[MIX_INPUT].getVoltage() / 10.f * params[MIX_CV_PARAM].getValue();
+		//mix = clamp(mix, 0.f, 1.f);
+		float out = crossfade(in, wet, mix);
+		outputs[ONE_OUTPUT].setVoltage(out);
+
+		// Clock light
+		// clockPhase += freq * args.sampleTime;
+		// if (clockPhase >= 1.f) {
+		// 	clockPhase -= 1.f;
+		// 	lights[CLOCK_LIGHT].setBrightness(1.f);
+		// }
+		// else {
+		// 	lights[CLOCK_LIGHT].setBrightnessSmooth(0.f, args.sampleTime);
+		// }
 	}
 };
 
